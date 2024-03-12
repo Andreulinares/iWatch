433711854c4f3625f1ca125334026e70c8bb75f6
Andreu <andreulinaresfp@ibf.cat>
2024-03-12 19:20:48 +0100

    estructura diseño vistas peliculas y series, listado categorias

--- resources/js/views/films/index.vue
+++ resources/js/views/films/index.vue
@@ -1,3 +1,55 @@
 <template>
-    <h1>HOLA</h1>
+    <div class="cont-principal">
+
+        <div v-for="category in categories.data" :key="category.id" class="texto-1">
+            {{ category.name }}
+        </div>
+
+        <!--<div class="filtro">
+            <label>Buscar:</label>
+            <input v-model="filtro" type="text" />
+            <button @click="buscarPelicula">Buscar</button>
+        </div>-->
+        <div class="buscador">
+            <input v-model="search_global" type="text" placeholder="Buscar...">
+        </div>
+        
+    </div>
 </template>
+
+<script>
+    import { ref, onMounted } from 'vue';
+    import useCategories from "@/composables/categories";
+
+    export default {
+        setup() {
+            const { categories, getCategories, getCategoryList } = useCategories();
+
+            onMounted(() => {
+            getCategories();
+            getCategoryList();
+            console.log(categories);
+            });
+
+            return {
+            categories
+            };
+        },
+    };
+
+
+</script>
+
+<style>
+    .cont-principal{
+        background-color: black;
+    }
+    .texto-1{
+        color: white;
+        font-size: 25px;
+    }
+    .buscador{
+
+    }
+
+</style>
--- resources/js/views/series/index.vue
+++ resources/js/views/series/index.vue
@@ -1,3 +1,45 @@
 <template>
-    <h1>SERIES</h1>
+    <div class="cont-principal">
+
+        <div v-for="category in categories.data" :key="category.id" class="texto-1">
+            {{ category.name }}
+        </div>
+
+        <!--<div class="filtro">
+            <label>Buscar:</label>
+            <input v-model="filtro" type="text" />
+            <button @click="buscarPelicula">Buscar</button>
+        </div>-->
+        <div class="buscador">
+            <input v-model="search_global" type="text" placeholder="Buscar...">
+        </div>
+        
+    </div>
 </template>
+
+<script>
+    import { ref, onMounted } from 'vue';
+    import useCategories from "@/composables/categories";
+
+    export default {
+        setup() {
+            const { categories, getCategories, getCategoryList } = useCategories();
+
+            onMounted(() => {
+            getCategories();
+            getCategoryList();
+            console.log(categories);
+            });
+
+            return {
+            categories
+            };
+        },
+    };
+
+
+</script>
+
+<style>
+
+</style>
