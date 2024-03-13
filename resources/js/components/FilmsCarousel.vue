96ab1498c1e1d8e982a373e535deef92d826ae66
Joel <joelcospfp@ibf.cat>
2024-03-13 19:01:34 +0100

    ---

--- resources/js/components/FilmsCarousel.vue
+++ resources/js/components/FilmsCarousel.vue
@@ -6,6 +6,7 @@
       <div id="scrollCont" class="scrolling-container p-0">
           <div class="scrolling-card" v-for="film in peliculas" :key="film.id">
             <img @click="handleImageClick(film.id)" :id="film.id" class="image-item"  data-bs-toggle="modal" data-bs-target="#staticBackdrop" width="350" :src="film.media.length > 0 ? film.media[0].original_url : '/images/placeholder.jpg'" :alt="film.name" style="max-height: 100px;">
+            <p>{{ film.categoria_id }}</p>
           </div>
       </div>
 
