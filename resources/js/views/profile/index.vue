060e9250777d67ae62f2ef497251ec45d506686c
JoelDAW2 <145133218+JoelDAW2@users.noreply.github.com>
2024-03-20 22:56:46 +0100

    + estilos

--- resources/js/views/profile/index.vue
+++ resources/js/views/profile/index.vue
@@ -1,63 +1,223 @@
 <template>
-    <h2 class="t-profile">EDITAR PERFIL</h2>
-    <div class="container">
-        
-    </div>
-    <div>
-    <!-- Icono del perfil -->
-    <i class="pi pi-user" @click="showGallery = true"></i>
-
-    <!-- Dialogo para la galería de imágenes -->
-        <Dialog v-model="showGallery" header="Selecciona una foto de perfil">
-            <div class="p-grid">
-            <!-- Iterar sobre las fotos de perfil -->
-                <div v-for="photo in photos" :key="photo.id" class="p-col-3">
-                    <img :src="photo.url" alt="Photo" @click="selectProfilePhoto(photo)">
+    <div class="card border-0 bg-primary mainBox">
+        <div class="card-body">
+            <form @submit.prevent="submitForm">
+                <div class="all d-flex justify-content-center align-items-center flex-wrap">
+                    <div class="mb-3">
+                        <label for="profile_image" class="form-label">Foto de perfil</label>
+                        <input type="file" class="form-control" id="profile_image" @change="onFileChange($event)">
+                        <div class="text-danger mt-1">
+                            {{ errors.profile_image }}
+                        </div>
+                        <div class="text-danger mt-1">
+                            <div v-for="message in validationErrors?.profile_image">
+                                {{ message }}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="mb-3 users d-flex">
+                            <div class="me-3">
+                                <input type="text" v-model="profile.name" class="form-control reqInfo" id="name" placeholder="Nombre">
+                                <div class="text-danger mt-1">
+                                    {{ errors.name }}
+                                </div>
+                                <div class="text-danger mt-1">
+                                    <div v-for="message in validationErrors?.name">
+                                        {{ message }}
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <input type="text" v-model="profile.apellido" class="form-control reqInfo" id="apellido" placeholder="Apellido">
+                                <div class="text-danger mt-1">
+                                    {{ errors.apellido }}
+                                </div>
+                                <div class="text-danger mt-1">
+                                    <div v-for="message in validationErrors?.apellido">
+                                        {{ message }}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <input type="text" v-model="profile.phone" class="form-control reqInfo" id="phone" placeholder="Teléfono">
+                            <div class="text-danger mt-1">
+                                {{ errors.phone }}
+                            </div>
+                            <div class="text-danger mt-1">
+                                <div v-for="message in validationErrors?.phone">
+                                    {{ message }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <input type="email" v-model="profile.email" class="form-control reqInfo" id="email" placeholder="Correo electrónico">
+                            <div class="text-danger mt-1">
+                                {{ errors.email }}
+                            </div>
+                            <div class="text-danger mt-1">
+                                <div v-for="message in validationErrors?.email">
+                                    {{ message }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <button :disabled="isLoading" class="btn btn-primary accion">
+                                <div v-show="isLoading" class=""></div>
+                                <span v-if="isLoading">Procesando...</span>
+                                <span v-else>Guardar</span>
+                            </button>
+                            <button>Eliminar cuenta</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </Dialog>
+            </form>
+        </div>
     </div>
+
+    <!-- SPACE -->
+    
+    <!-- 
+    <div class="card border-0 bg-primary">
+        <div class="card-body">
+            <form @submit.prevent="submitForm">
+                <div class="mb-3">
+                    <label for="profile_image" class="form-label">Foto de perfil</label>
+                    <input type="file" class="form-control" id="profile_image" @change="onFileChange($event)">
+                    <div class="text-danger mt-1">
+                        {{ errors.profile_image }}
+                    </div>
+                    <div class="text-danger mt-1">
+                        <div v-for="message in validationErrors?.profile_image">
+                            {{ message }}
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nombre</label>
+                    <input type="text" v-model="profile.name" class="form-control" id="name">
+                    <div class="text-danger mt-1">
+                        {{ errors.name }}
+                    </div>
+                    <div class="text-danger mt-1">
+                        <div v-for="message in validationErrors?.name">
+                            {{ message }}
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Apellido</label>
+                    <input type="text" v-model="profile.apellido" class="form-control" id="apellido">
+                    <div class="text-danger mt-1">
+                        {{ errors.apellido }}
+                    </div>
+                    <div class="text-danger mt-1">
+                        <div v-for="message in validationErrors?.apellido">
+                            {{ message }}
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Teléfono</label>
+                    <input type="text" v-model="profile.phone" class="form-control" id="phone">
+                    <div class="text-danger mt-1">
+                        {{ errors.phone }}
+                    </div>
+                    <div class="text-danger mt-1">
+                        <div v-for="message in validationErrors?.phone">
+                            {{ message }}
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Correo</label>
+                    <input type="email" v-model="profile.email" class="form-control" id="email">
+                    <div class="text-danger mt-1">
+                        {{ errors.email }}
+                    </div>
+                    <div class="text-danger mt-1">
+                        <div v-for="message in validationErrors?.email">
+                            {{ message }}
+                        </div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <button :disabled="isLoading" class="btn btn-primary">
+                        <div v-show="isLoading" class=""></div>
+                        <span v-if="isLoading">Procesando...</span>
+                        <span v-else>Guardar</span>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div> -->
 </template>
 
-<script>
-import { ref, onMounted } from "vue";
-import Dialog from "primevue/dialog";
+<script setup>
+import { onMounted, reactive, watchEffect } from "vue";
+import { useForm, useField, defineRule } from "vee-validate";
+import { required, min } from "@/validation/rules"
 import useProfile from "@/composables/profile";
+defineRule('required', required)
+// defineRule('email', email)
+defineRule('min', min);
 
-export default {
-    components: {
-    Dialog
-    },
-    setup() {
-
-        const { profile: profileData, getProfile, updateProfile, validationErrors, isLoading } = useProfile()
-
-        const showGallery = ref(false);
-        const photos = ref([]);
-
-        const selectProfilePhoto = (photo) => {
-        // Aquí puedes implementar la lógica para cambiar la foto de perfil
-            console.log('Seleccionaste la foto de perfil:', photo);
-        // Por ejemplo, puedes emitir un evento o llamar a una función para cambiar la foto de perfil
-        };
-
-        return {
-            showGallery,
-            photos,
-            selectProfilePhoto
-        };
-
-        onMounted(() => {
-            getProfile()
-        })
+    const schema = {
+        name: 'required|min:3',
+        email: 'required',
     }
-};
+    // Create a form context with the validation schema
+    const { validate, errors } = useForm({ validationSchema: schema })
+    // Define actual fields for validation
+    const { value: name } = useField('name', null, { initialValue: '' });
+    const { value: email } = useField('email', null, { initialValue: '' });
+    const { value: apellido } = useField('apellido', null, { initialValue: '' });
+    const { value: phone } = useField('phone', null, { initialValue: '' });
+    const { value: profile_image } = useField('profile_image', '', { initialValue: '' });
+    const { profile: profileData, getProfile, updateProfile, validationErrors, isLoading } = useProfile()
+    const profile = reactive({
+        name,
+        email,
+        apellido,
+        phone,
+        profile_image
+    })
+    function submitForm() {
+        validate().then(form => { if (form.valid) updateProfile(profile) })
+    }
+    
+
+    function onFileChange(event) {
+    const file = event.target.files[0]; // Obtener el archivo seleccionado
+    const formData = new FormData();
+    formData.append('profile_image', file);
+
+    // Envía la imagen al backend utilizando Axios
+    axios.post('/api/user', formData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })
+    .then(response => {
+        // Maneja la respuesta del backend
+        console.log('Imagen cargada exitosamente:', response.data.url);
+        profile.profile_image = response.data.url; // Actualiza la URL de la imagen en el formulario
+    })
+    .catch(error => {
+        console.error('Error al cargar la imagen:', error);
+    });
+}
+
+    onMounted(() => {
+        getProfile()
+    })
+
+    watchEffect(() => {
+        profile.name = profileData.value.name
+        profile.email = profileData.value.email
+        profile.apellido = profileData.value.apellido
+        profile.phone = profileData.value.phone
+        profile.profile_image = profileData.value.profile_image
+    })
 </script>
-
-<style>
-    .t-profile{
-        margin-top: 60px;
-        text-align: center;
-    }
-
-</style>
